529397a45499f0ffc03a575135bf18f2a6cb6ca7
Prachi123521 <Prachisharmahp02@gmail.com>
2025-03-12 19:45:39 +0530

    made header component

--- figma-app/src/app/component/Header.tsx
+++ figma-app/src/app/component/Header.tsx
@@ -2,13 +2,32 @@ const Header = () =>(
     
 
         
-     <div className="mx-6 bg-stone-700 px-2 text-slate-50">
-        <div className="grid">
-        <p >
+     <div className=" grid grid-cols-1 divide-y-4 border-purple-600">
+        <p  className=" bg-slate-700 text-slate-50">
             Get early access on launches and offers.Sign Up For Texts
         </p>
-
+    
+         <div>
+            <div className="float-left mr-2 pl-25">Women</div>
+            <div className="float-left mr-2">Men</div>
+            <div className="float-left mr-2">About</div>
+            <div className="float-left ">Everworld Stories</div>
+            <div className=" text-center font-bold pr-100">
+            <h1>E V E R L A N E</h1>
     </div>
+      <div className="float-left mr-2 pl-15">Holiday Gifting</div>
+      <div className="float-left mr-2">New Arrivals</div>
+      <div className="float-left mr-2">Best-Sellers</div>
+      <div className="float-left mr-2">Clothing</div>
+      <div className="float-left mr-2">Tops & Sweaters</div>
+      <div className="float-left mr-2">Pants & Jeans</div>
+      <div className="float-left mr-2">Outerwear</div>
+      <div className="float-left mr-2">Shoes & Bags</div>
+      <div className="float-left mr-2">Sale</div>
+
+            
+         </div>
+         
   </div>
 
 
