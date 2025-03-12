3230b97562c3cd0a6961774f1ce5164f54afedb4
Prachi123521 <Prachisharmahp02@gmail.com>
2025-03-12 18:37:12 +0530

    first commit

---
+++ figma-app/src/app/component/Header.tsx
@@ -0,0 +1,20 @@
+const Header = () =>(
+    
+
+        
+     <div className="mx-6 bg-stone-700 px-2 text-slate-50">
+        <div className="grid">
+        <p >
+            Get early access on launches and offers.Sign Up For Texts
+        </p>
+
+    </div>
+  </div>
+
+
+
+
+    
+)
+
+export default Header;
